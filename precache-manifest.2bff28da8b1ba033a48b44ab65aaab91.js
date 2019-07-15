7147b820e9a1d509abb16b7570a54c34bf76321f
Ivan Deiko <IvanDeiko97@gmail.com>
2019-07-15 15:35:45 +0300

    'first'

---
+++ precache-manifest.2bff28da8b1ba033a48b44ab65aaab91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44cd7f233e7351945c20f102fd68a680",
+    "url": "./index.html"
+  },
+  {
+    "revision": "69012a5d8e10019070af",
+    "url": "./static/css/main.e0285e9f.chunk.css"
+  },
+  {
+    "revision": "2857c2d8bea0049103b8",
+    "url": "./static/js/2.55543a50.chunk.js"
+  },
+  {
+    "revision": "69012a5d8e10019070af",
+    "url": "./static/js/main.f795b42e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
