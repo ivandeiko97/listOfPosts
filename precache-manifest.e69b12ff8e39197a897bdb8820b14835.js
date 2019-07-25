1b64d3391f2bb08e5a5d6dbf63345cac4dd66780
Ivan Deiko <IvanDeiko97@gmail.com>
2019-07-25 12:22:55 +0300

    'solution'

--- precache-manifest.e69b12ff8e39197a897bdb8820b14835.js
+++ precache-manifest.e69b12ff8e39197a897bdb8820b14835.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "44cd7f233e7351945c20f102fd68a680",
+    "revision": "371786986eaa4003fb76663d842e636c",
     "url": "./index.html"
   },
   {
-    "revision": "69012a5d8e10019070af",
+    "revision": "06ad849ae749fe4e2fb6",
     "url": "./static/css/main.e0285e9f.chunk.css"
   },
   {
-    "revision": "2857c2d8bea0049103b8",
-    "url": "./static/js/2.55543a50.chunk.js"
+    "revision": "4758c86e31a05e7cda83",
+    "url": "./static/js/2.600a84be.chunk.js"
   },
   {
-    "revision": "69012a5d8e10019070af",
-    "url": "./static/js/main.f795b42e.chunk.js"
+    "revision": "06ad849ae749fe4e2fb6",
+    "url": "./static/js/main.916c8a91.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
